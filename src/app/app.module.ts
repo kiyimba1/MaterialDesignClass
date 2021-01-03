7a0ac38534d4ddc6a023849ec9da6102a8479289
Edwin Kiyimba <edwineddie54@gmail.com>
2021-01-03 12:01:51 +0300

    Added Material Design and Animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+// import { NoopAnimationsModule } from '@angular/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    // NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
